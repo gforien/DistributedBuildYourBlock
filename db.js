c81086b782ff509b74d10613970c524be9e03061
Gouaty Chloe <cgouaty@TC405-109-05.insa-lyon.fr>
2020-01-09 09:09:32 +0100

    serveurs qui se configurent :sparkles:

--- db.js
+++ db.js
@@ -1,4 +1,20 @@
-const PORT = 3000;
+/* chargement de la configuration */
+const path = require("path");
+
+const getConfig = function getConfig() {
+  if (process.argv.length < 3) {
+    console.error("Vous devez indiquer le fichier de configuration");
+    console.info(`Usage : node ${process.argv[1]} <fileName>`);
+    throw new Error("Need config filename");
+  } else {
+    return require(path.resolve(process.argv[2]));
+  }
+}
+const config = getConfig();
+console.log("config", config);
+
+/* serveur */
+const PORT = config.port;
 
 const Server = require('socket.io');
 const io = new Server(PORT, {
