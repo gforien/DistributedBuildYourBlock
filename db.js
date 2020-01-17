3cd2a4b4c40b00251a57e75074029e5f306cf11a
Gouaty Chloe <cgouaty@TC405-114-11.insa-lyon.fr>
2020-01-17 09:48:43 +0100

    synchronisation entre serveurs : on voit de multiples setServ

--- db.js
+++ db.js
@@ -29,39 +29,66 @@ const srvs = config.autres.map((element, index) => {
     var s = Client(`http://localhost:${element}`, {path: '/dbyb'});
     s.on('connect', () => {
       console.log('Connection établie avec le serveur '+element);
-      //console.log(srvs);
     });
     return s;
 });
 
 const db = Object.create(null);
+
+srvs[0].emit('keys', (serv_db) => {
+  serv_db.forEach( (cle) => {
+    srvs[0].emit('getServ', cle, PORT, (valeur) => {
+      console.log(`getServ ${cle} => ${valeur}`);
+      db[cle] = valeur;
+    });
+  })
+});
+
 io.on('connect', (socket) => {
   console.log('Nouvelle connexion');
 
   socket.on('get', function(field, callback){
-    console.log(`get ${field}: ${db[field]}`);
+    console.log(`get ${field} => ${db[field]}`);
     callback(db[field]);
   });
 
   socket.on('set', function(field, value, callback){
     if (field in db) {
       console.log(`set error : Field ${field} exists.`);
-      // db[field] = value;
       callback(false);
     } else {
-      console.log(`set ${field} : ${value}`);
+      console.log(`set ${field} to ${value}`);
       db[field] = value;
       callback(true);
 
       // synchronise
       srvs.forEach((s) => {
+        console.log(`setServ ${field} to ${value}`);
         s.emit('setServ', field, value);
       });
     }
+  });
+
+  socket.on('keys', function(callback){
+    console.log(`keys => ${Object.keys(db)}`);
+    callback(Object.keys(db));
   });
 
   socket.on('setServ', function(field, value){
     console.log(`SETSERV ${field} : ${value}`);
     db[field] = value;
   });
+
+  socket.on('getServ', function(field, port, callback){
+    console.log(`GETSERV ${field}`);
+
+    var s2 = Client(`http://localhost:${port}`, {path: '/dbyb'});
+    s2.on('connect', () => {
+      console.log('Connection établie avec le serveur '+port);
+    });
+    srvs.push(s2);
+
+    callback(db[field]);
+  });
+
 });
